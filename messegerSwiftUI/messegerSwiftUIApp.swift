63bc6e97e469bfb365caffba51c338723e560162
Stas <staszhingel@icloud.com>
2021-08-24 22:05:43 +0300

    Initial Commit

---
+++ messegerSwiftUI/messegerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  messegerSwiftUIApp.swift
+//  messegerSwiftUI
+//
+//  Created by Стас Жингель on 24.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct messegerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
